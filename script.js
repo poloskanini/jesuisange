54cb89766642597e3830137ce213746a142b74c8
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-05-31 11:09:38 +0200

    Version 1.0

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 setTimeout(() => {
   document.querySelector('.loader').style.display = 'none';
 }, 6500)
+
+// Container Scores
+let wrapperTop = document.querySelector('.wrapper');
+
+const launch = () => {
+  let valueDisplays = document.querySelectorAll('.num');
+
+  // Rallonger interval pour ralentir le compteur
+  let interval = 2500;
+
+  valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+
+    let counter = setInterval(function() {
+      startValue += 1;
+      valueDisplay.textContent = startValue;
+      if(startValue == endValue) {
+        clearInterval(counter);
+      }
+    }, duration)
+  })
+}
+
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: .5,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  for(const entry of entries) {
+    if(entry.isIntersecting) {
+      entry.target.classList.add("is-visible");
+      launch();
+      observer.unobserve(entry.target);
+    }
+  }
+}, options)
+
+observer.observe(wrapperTop);
